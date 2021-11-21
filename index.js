d1f21c77897a387ca3b4ccdfab30a4796c2dfd17
sho <sho_ws@outlook.com>
2021-11-22 00:10:05 +0900

    ファイルから読み込む

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let faceapi;
 let video;
+let videoWidth;
+let videoHeight;
 let detections;
+let cameraPosition;
 
 const detection_options = {
     withLandmarks: true,
@@ -8,13 +11,19 @@ const detection_options = {
 }
 
 function setup() {
-  createCanvas(640, 380);
-  video = createCapture(VIDEO);
+  // カメラから読み込む
+  // video = createCapture(VIDEO);
+  // ファイルから読み込む
+  video = createVideo('http://localhost:8000/ViewTracker/img/mr_fuji.mp4');
+  video.elt.onloadeddata = function () {
+    videoWidth = video.width;
+    videoHeight = video.height;
+    const canvas = createCanvas(videoWidth, videoHeight + 200);
+    canvas.parent('canvas');
+  };
   video.elt.muted = true;
-  video.loop();
-  video.size(width, height);
-  faceapi = ml5.faceApi(video, detection_options, modelReady)
-  textAlign(RIGHT);
+  video.hide();
+  faceapi = ml5.faceApi(video.loop(), detection_options, modelReady)
 }
 
 function modelReady() {
@@ -24,91 +33,117 @@ function modelReady() {
 function gotResults(err, result) {
   if (err) {
       console.log(err)
-      return
-  }
-  console.log(result)
+      faceapi.detect(gotResults)
+    }
   detections = result;
   background(255);
-  image(video, 0,0, width, height)
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  image(video, 0,0, videoWidth, videoHeight)
   if (detections) {
       if (detections.length > 0) {
           drawBox(detections)
           drawLandmarks(detections)
       }
   }
+  pop();
+  switchCamera();
   faceapi.detect(gotResults)
 }
 
 function drawBox(detections){
-    for(let i = 0; i < detections.length; i++){
-        const alignedRect = detections[i].alignedRect;
-        const x = alignedRect._box._x
-        const y = alignedRect._box._y
-        const boxWidth = alignedRect._box._width
-        const boxHeight  = alignedRect._box._height
-        
-        noFill();
-        stroke(161, 95, 251);
-        strokeWeight(2);
-        rect(x, y, boxWidth, boxHeight);
-    }
+  for(let i = 0; i < detections.length; i++){
+    const alignedRect = detections[i].alignedRect;
+    const x = alignedRect._box._x
+    const y = alignedRect._box._y
+    const boxWidth = alignedRect._box._width
+    const boxHeight  = alignedRect._box._height
     
+    noFill();
+    stroke(161, 95, 251);
+    strokeWeight(2);
+    rect(x, y, boxWidth, boxHeight);
+  }  
 }
 
 function drawLandmarks(detections){
-    noFill();
-    stroke(161, 95, 251)
-    strokeWeight(2)
-
-    for(let i = 0; i < detections.length; i++){
-        const mouth = detections[i].parts.mouth; 
-        const nose = detections[i].parts.nose;
-        const leftEye = detections[i].parts.leftEye;
-        const rightEye = detections[i].parts.rightEye;
-        const rightEyeBrow = detections[i].parts.rightEyeBrow;
-        const leftEyeBrow = detections[i].parts.leftEyeBrow;
-
-        console.log('leftEye: ' + leftEye[0]._x, ', ' + leftEye[3]._y);
-        console.log('nose: ' + nose[0]._x + ', ' + nose[0]._y);
-        console.log('rightEye: ' + rightEye[3]._x + ', ' + rightEye[0]._y);
-        
-        const leftEyeToNose = Math.abs(leftEye[0]._x - nose[0]._x);
-        const NoseToRightEye = Math.abs(nose[0]._x - rightEye[0]._x);
-
-        if (leftEyeToNose / NoseToRightEye < 3/5) {
-            console.log('下手');
-        } else if (leftEyeToNose / NoseToRightEye < 5/3) {
-            console.log('上手');
-        } else {
-            console.log('正面');
-        }
-        
-        
-
-        drawPart(mouth, true);
-        drawPart(nose, false);
-        drawPart(leftEye, true);
-        drawPart(leftEyeBrow, false);
-        drawPart(rightEye, true);
-        drawPart(rightEyeBrow, false);
-
+  noFill();
+  stroke(161, 95, 251)
+  strokeWeight(2)
+  for(let i = 0; i < detections.length; i++){
+    const mouth = detections[i].parts.mouth; 
+    const nose = detections[i].parts.nose;
+    const leftEye = detections[i].parts.leftEye;
+    const rightEye = detections[i].parts.rightEye;
+    const rightEyeBrow = detections[i].parts.rightEyeBrow;
+    const leftEyeBrow = detections[i].parts.leftEyeBrow;
+    // console.log('leftEye: ' + leftEye[0]._x, ', ' + leftEye[3]._y);
+    // console.log('nose: ' + nose[0]._x + ', ' + nose[0]._y);
+    // console.log('rightEye: ' + rightEye[3]._x + ', ' + rightEye[0]._y);
+    
+    const leftEyeToNose = Math.abs(leftEye[0]._x - nose[0]._x);
+    const NoseToRightEye = Math.abs(nose[0]._x - rightEye[0]._x);
+    if (leftEyeToNose / NoseToRightEye < 3/5) {
+        console.log('下手');
+        cameraPosition = 0;
+    } else if (leftEyeToNose / NoseToRightEye < 5/3) {
+        console.log('上手');
+        cameraPosition = 1;
+    } else {
+        console.log('正面');
+        cameraPosition = 2;
     }
+    
+    drawPart(mouth, true);
+    drawPart(nose, false);
+    drawPart(leftEye, true);
+    drawPart(leftEyeBrow, false);
+    drawPart(rightEye, true);
+    drawPart(rightEyeBrow, false);
+  }
 
 }
 
 function drawPart(feature, closed){
-    
-    beginShape();
-    for(let i = 0; i < feature.length; i++){
-        const x = feature[i]._x
-        const y = feature[i]._y
-        vertex(x, y)
-    }
-    
-    if(closed === true){
-        endShape(CLOSE);
-    } else {
-        endShape();
-    }
-    
+  beginShape();
+  for(let i = 0; i < feature.length; i++){
+    const x = feature[i]._x
+    const y = feature[i]._y
+    vertex(x, y)
+  }
+  
+  if(closed === true){
+    endShape(CLOSE);
+  } else {
+    endShape();
+  }
+}
+
+function switchCamera() {
+  push();
+  fill(60, 60, 60);
+  translate(50, videoHeight + 100);
+  rect(0, 0, 140, 50);
+  translate(150, 0);
+  rect(0, 0, 140, 50);
+  translate(150, 0);
+  rect(0, 0, 140, 50);
+  pop();
+
+  textAlign(CENTER, CENTER);
+  textSize(20);
+  fill(255, 255, 255);
+  if (cameraPosition === 2) {
+    text('下手カメラ', 120, videoHeight + 125);
+    return;
+  }
+  if (cameraPosition === 1) {
+    text('正面カメラ', 270, videoHeight + 125);
+    return;
+  }
+  if (cameraPosition === 0) {
+    text('上手カメラ', 420, videoHeight + 125);
+    return;
+  }
 }
